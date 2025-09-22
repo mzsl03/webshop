dda813fe11bb75bda676681afd909d4f6401a4a9
Zoli <olahzoli19@gmail.com>
2025-09-22 19:16:05 +0200

    create new field to Products table

--- core_webshop/core_webshop/settings.py
+++ core_webshop/core_webshop/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = "core_webshop.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'phoneshop',
+        'USER': 'postgres',
+        'PASSWORD': 'Zoli',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
--- core_webshop/phoneshop/models.py
+++ core_webshop/phoneshop/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
 class Products(models.Model):
+
+    categories = (
+        (1, "Telefon"),
+        (2, "Tartozék")
+    )
+    
     name = models.CharField(max_length=255)
     price = models.IntegerField()
+    category = models.CharField(choices=categories, default=1)
