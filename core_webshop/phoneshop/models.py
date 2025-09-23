6a5519899ec13c05d173b2799929babaa7fada0f
Zoli <olahzoli19@gmail.com>
2025-09-23 12:45:00 +0200

    feat: add Products table to database

--- core_webshop/phoneshop/models.py
+++ core_webshop/phoneshop/models.py
@@ -1,12 +1,33 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 class Products(models.Model):
 
     categories = (
-        (1, "Telefon"),
-        (2, "Tartozék")
+        ("1", "Telefon"),
+        ("2", "Tartozék")
     )
     
     name = models.CharField(max_length=255)
     price = models.IntegerField()
-    category = models.CharField(choices=categories, default=1)
+    category = models.CharField(choices=categories, default='1')
+    colors = ArrayField(
+        models.CharField(max_length=50),
+        blank=False,
+        default=list
+    )
+    image_path = ArrayField(
+        models.CharField(max_length=255),
+        blank=True,
+        default=list
+    )
+    available = ArrayField(
+        models.IntegerField(),
+        blank=True,
+        default=list
+    )
+    prices=ArrayField(
+        models.IntegerField(),
+        blank=False,
+        default=list
+    )
