5c90ce34f6bff9171fed5cdeddf9b509385e367c
Molnar Zsolt Levente <molzsolti03@mailbox.unideb.hu>
2025-09-23 11:07:51 +0200

    feat: add database server connection to project

--- core_webshop/core_webshop/settings.py
+++ core_webshop/core_webshop/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = "core_webshop.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'phoneshop',
+        'USER': 'admin',
+        'PASSWORD': 'M9v!e#Qx7Tp@2Zr',
+        'HOST': 'dnsszm.go.ro', 
+        'PORT': '5432',
     }
 }
 
