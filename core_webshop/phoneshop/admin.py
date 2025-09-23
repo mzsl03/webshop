80a7d77380f0d05c12ad3746640f21df2a043bc6
Zoli <olahzoli19@gmail.com>
2025-09-23 18:33:04 +0200

    feat: register Storage in admin

--- core_webshop/phoneshop/admin.py
+++ core_webshop/phoneshop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Products, Shops, Specs, Workers, Orders, Users
+from .models import Products, Shops, Specs, Storage, Workers, Orders, Users
 
 admin.site.register(Products)
 admin.site.register(Specs)
@@ -7,3 +7,4 @@ admin.site.register(Shops)
 admin.site.register(Workers)
 admin.site.register(Orders)
 admin.site.register(Users)
+admin.site.register(Storage)
