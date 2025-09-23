e2a61f7ca4ed201fc2e3a8cdbb8ad9c67246c8be
Zoli <olahzoli19@gmail.com>
2025-09-23 19:07:49 +0200

    feat: register Cart in admin

--- core_webshop/phoneshop/admin.py
+++ core_webshop/phoneshop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Products, Sales, Shops, Specs, Storage, Workers, Orders, Users
+from .models import Cart, Products, Sales, Shops, Specs, Storage, Workers, Orders, Users
 
 admin.site.register(Products)
 admin.site.register(Specs)
@@ -9,3 +9,4 @@ admin.site.register(Orders)
 admin.site.register(Users)
 admin.site.register(Storage)
 admin.site.register(Sales)
+admin.site.register(Cart)
