b3c7b122df8f96803c5708936b12106f99034afa
Zoli <olahzoli19@gmail.com>
2025-09-23 18:16:04 +0200

    feat: register Users in admin

--- core_webshop/phoneshop/admin.py
+++ core_webshop/phoneshop/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Products, Shops, Specs, Workers, Orders
+from .models import Products, Shops, Specs, Workers, Orders, Users
 
 admin.site.register(Products)
 admin.site.register(Specs)
 admin.site.register(Shops)
 admin.site.register(Workers)
 admin.site.register(Orders)
+admin.site.register(Users)
