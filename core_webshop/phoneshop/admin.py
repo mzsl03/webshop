b2e225efb1abff5ccf58fca649c35ca3c669f619
Zoli <olahzoli19@gmail.com>
2025-09-23 18:54:59 +0200

    feat: register Sales in admin

--- core_webshop/phoneshop/admin.py
+++ core_webshop/phoneshop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Products, Shops, Specs, Storage, Workers, Orders, Users
+from .models import Products, Sales, Shops, Specs, Storage, Workers, Orders, Users
 
 admin.site.register(Products)
 admin.site.register(Specs)
@@ -8,3 +8,4 @@ admin.site.register(Workers)
 admin.site.register(Orders)
 admin.site.register(Users)
 admin.site.register(Storage)
+admin.site.register(Sales)
