88879b5269c1da6a4e5b34d9f423aac8bb6d07e5
Molnar Zsolt Levente <molzsolti03@mailbox.unideb.hu>
2025-09-24 10:39:29 +0200

    feat: loging in feature now functionnal using django.contrib.auth modul

--- core_webshop/phoneshop/views.py
+++ core_webshop/phoneshop/views.py
@@ -1,15 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login as auth_login
+from django.shortcuts import render, redirect
 from .models import Products
 
+@login_required(login_url='/')
 def index(request):
     products = Products.objects.all()
+    print("DEBUG: home view futott", request.user)
     return render(request, 'index.html', {"products": products})
 
+@login_required(login_url='/')
 def cart(request):
     return render(request, 'cart.html')
 
+@login_required(login_url='/')
 def receipts(request):
     return render(request, 'receipts.html')
 
+
 def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth_login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {'error': 'Hibás felhasználónév vagy jelszó!'})
     return render(request, 'login.html')
