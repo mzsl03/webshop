5d7f73e0efb9d66ef84b791ba984ba4bd0c934ba
Molnar Zsolt Levente <molzsolti03@mailbox.unideb.hu>
2025-09-26 16:27:55 +0200

    fix: path to images are corrected in form

--- core_webshop/support_files/add_prod.py
+++ core_webshop/support_files/add_prod.py
@@ -2,8 +2,8 @@ from django import forms
 from phoneshop.models import Products
 from django.utils.text import slugify
 
-class ProductForm(forms.ModelForm):
 
+class ProductForm(forms.ModelForm):
     class Meta:
         model = Products
         fields = ['name', 'price', 'category', 'colors']
@@ -18,17 +18,26 @@ class ProductForm(forms.ModelForm):
         colors = self.cleaned_data['colors']
         if isinstance(colors, str):
             colors = [color.strip() for color in colors.split(',') if color.strip()]
+        if not isinstance(colors, list):
+            raise forms.ValidationError("A színeknek listának kell lenniük.")
         return colors
+
+    def clean_prices(self):
+        prices = self.cleaned_data['prices']
 
     def clean_name(self):
         name = self.cleaned_data['name']
         slug_name = slugify(name)
-        self.cleaned_data['image_path'] = [f"{slug_name}.png"]
+        colors = self.cleaned_data.get('colors', [])
+        self.cleaned_data['image_path'] = [f"{slug_name}-{color}.png" for color in colors]
         return name
+
 
     def save(self, commit=True):
         instance = super().save(commit=False)
-        instance.image_path = self.cleaned_data.get('image_path', [])
+        slug_name = slugify(self.cleaned_data['name'])
+        colors = self.cleaned_data.get('colors', [])
+        instance.image_path = [f"{slug_name}-{color}.png" for color in colors]
         if commit:
             instance.save()
         return instance
