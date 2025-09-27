8180a059358c4a816f04d1d7bfa1f165cc972e4b
Abari Gergő <agergoegyetem@mailbox.unideb.hu>
2025-09-27 20:23:32 +0200

    fix: add new logic to change available field

--- core_webshop/support_files/add_cart.py
+++ core_webshop/support_files/add_cart.py
@@ -4,8 +4,10 @@ from phoneshop.models import Cart
 def increment_cart_item(shop, worker_obj, p_name, color, storage):
     cart_items = Cart.objects.all()
     for item in cart_items:
+        if (storage != None):
+            storage = int(storage)
         name = item.user.worker
-        if (item.shop == shop and name == worker_obj and item.product == p_name and item.color == color and item.storage == int(storage)):
+        if (item.shop == shop and name == worker_obj and item.product == p_name and item.color == color and item.storage == storage):
             item.quantity += 1
             item.price += item.product.price
             item.save()
