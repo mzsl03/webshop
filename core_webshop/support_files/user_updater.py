39301c5878b1312d0e6f95a719ccd02d624cd8ca
Molnar Zsolt Levente <molzsolti03@mailbox.unideb.hu>
2025-09-27 15:44:33 +0200

    fix: user password cannot be changed through the form

--- core_webshop/support_files/user_updater.py
+++ core_webshop/support_files/user_updater.py
@@ -11,24 +11,15 @@ class UserUpdateForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'password', 'is_active']
+        fields = ['username', 'first_name', 'last_name', 'email', 'is_active']
         labels = {
             'username': 'Felhasználónév',
             'first_name': 'Keresztnév',
             'last_name': 'Vezetéknév',
             'email': 'Email cím',
-            'password': 'Jelszó',
             'is_active': 'Aktív'
         }
         help_texts = {
             'username': 'Felhasználónév: csak betűk, számok és @/./+/-/_ karakterek engedélyezettek!',
             'is_active': 'A felhasználó aktiválása/deaktiválása'
         }
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        if self.cleaned_data['password']:
-            user.set_password(self.cleaned_data['password'])
-        if commit:
-            user.save()
-        return user
